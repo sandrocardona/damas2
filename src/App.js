6240cbe3140e086fa15d61f620fef5c4a923cf8c
sandrocardona <sandrocardona41@gmail.com>
2024-01-23 12:57:09 +0100

    23 enero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { Button} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Botonera(props) {
+  //COMPONENTE QUE RENDERIZA EL TABLERO
+  let campoAux = [];
+
+  for (let i = 0; i < props.campo.length; i++) {
+    let fila = [];
+    for (let j = 0; j < props.campo[i].length; j++) {
+      fila.push(<Button className='btnClass' color="secondary"></Button>)
+    }
+    campoAux.push(fila)
+    campoAux.push(<br></br>)
+  }
+  return campoAux;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campo: Array(8).fill(0),
+    }
+  }
+
+  componentWillMount() {
+    let arr = [];
+    for (let i = 0; i < 8; i++) {
+      let fila = [];
+      for (let j = 0; j < 8; j++) {
+        fila.push(0);
+      }
+      arr.push(fila);
+    }
+    console.log(arr);
+    this.setState({campo:arr});
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Botonera campo={this.state.campo} />
+          <h1>hola</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
